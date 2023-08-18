0d86ffaa7f349f39773595ea7e2e4909ce7c4555
Adrian Soovary <adrian.soovary@gmail.com>
2023-08-18 13:10:53 +0200

    feat: initialize monorepo with aws cdk basic template

---
+++ packages/infrastructure/test/aws-cdk-with-typescript-foundations.test.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+// import { AwsCdkWithTypescriptFoundationsStack } from '../lib/aws-cdk-with-typescript-foundations-stack';
+// import { Template } from 'aws-cdk-lib/assertions';
+import * as AwsCdkWithTypescriptFoundations from '../lib/aws-cdk-with-typescript-foundations-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-with-typescript-foundations-stack.ts
+test('diese', () => {
+  const app = new cdk.App()
+  const stack = new AwsCdkWithTypescriptFoundations.AwsCdkWithTypescriptFoundationsStack(app, 'MyTestStack')
+  expectCDK(stack).to(matchTemplate({}))
+})
