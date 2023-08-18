28f14abbb1d2bbc14e1569a0a10d76aa81ec2033
Adrian Soovary <adrian.soovary@gmail.com>
2023-08-18 15:07:00 +0200

    fix: jest dom version

--- packages/frontend/src/test/setup.ts
+++ packages/frontend/src/test/setup.ts
@@ -3,6 +3,7 @@ import { cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import matchers from "@testing-library/jest-dom/matchers";
 import fetch from "cross-fetch";
+import "@testing-library/jest-dom/extend-expect";
 
 declare global {
   var user: ReturnType<typeof userEvent.setup>;
--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   },
   test: {
     environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
+    setupFiles: path.resolve(__dirname, "./src/test/setup.ts"),
     globals: true,
   },
 });
